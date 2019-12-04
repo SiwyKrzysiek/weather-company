6c2baacbd116db80593a5a7b1ad33fbf59c64304
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2019-12-04 21:37:19 +0100

    Parse wather data

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+let weatherData;
+
 async function getWeatherData(cityName) {
     const apiKey = "7cad625ece0a657d34524777c3f14cfa";
     const urlBase = "https://api.openweathermap.org/data/2.5/weather"
@@ -10,17 +12,27 @@ async function getWeatherData(cityName) {
 }
 
 // Reload weather and update website
-async function displayWeather() {
+// Lousily couple with rendering by using view model
+async function displayWeather(cityName) {
     try {
-        let response = await getWeatherData('Warsaw');
-        console.log(response);
+        let response = await getWeatherData(cityName);
+        weatherData = await response.json();
+
+        renderWeather();
     }
     catch (e) {
         alert("Weather information is currently unavailable");
     }
 }
 
+// Display weather data in html
+// Weather date is loaded from weatherData variable
+function renderWeather() {
+
+}
+
 
 $(document).ready(() => {
-    displayWeather();
+    // TODO: Get city name from user/config
+    displayWeather('Warsaw');
 });
